7c61c6a5b40e8af0703b407e8476ce91344e2848
lilianegikundiro <liliane.gikundiro@amalitech.org>
2023-09-11 22:24:17 +0200

    Update CSRF cookie domain

--- photos/settings.py
+++ photos/settings.py
@@ -28,8 +28,8 @@ import os
 SECRET_KEY = 'django-insecure-5fjlbbqd*k1*2c7_6=x2ok!ch&83rpovciqt^u+=y=g*#fnh(c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-CSRF_COOKIE_DOMAIN = 'estolator.onrender.com'
+DEBUG = False
+CSRF_COOKIE_DOMAIN = 'trove-mh5l.onrender.com'
 
 ALLOWED_HOSTS = ["trove-mh5l.onrender.com"]
 
