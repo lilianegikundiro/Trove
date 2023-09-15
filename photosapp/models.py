034d66c90b3dbe5eaa77bfdcda973d84499de68a
lilianegikundiro <liliane.gikundiro@amalitech.org>
2023-09-15 14:00:48 +0200

    Add navigation links to login and registration pages

--- photosapp/models.py
+++ photosapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
+# from django.contrib.auth.models import User  
 
 # Create your models here.
 
@@ -7,7 +8,7 @@ from cloudinary.models import CloudinaryField
 
 class photos(models.Model):
    
-    
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     media = CloudinaryField(resource_type='auto')
     date= models.DateTimeField(default=now)
