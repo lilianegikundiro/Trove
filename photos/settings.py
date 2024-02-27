a58550ba8ce36ebe70a9e521a2e8f7702d22690f
lilianegikundiro <liliane.gikundiro@amalitech.org>
2024-02-28 00:12:23 +0200

    Changed cookie domain to none

--- photos/settings.py
+++ photos/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-5fjlbbqd*k1*2c7_6=x2ok!ch&83rpovciqt^u+=y=g*#fnh(c
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-CSRF_COOKIE_DOMAIN = "trove-mh5l.onrender.com"
+CSRF_COOKIE_DOMAIN = None
 
 ALLOWED_HOSTS = ["trove-mh5l.onrender.com","127.0.0.1"]
 USE_X_FORWARDED_HOST = True
